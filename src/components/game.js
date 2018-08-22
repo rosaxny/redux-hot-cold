839e20fe87c19bdd3beda32f0a89b388afbf4ddc
rosaxny <rosaxny@gmail.com>
2018-08-22 09:47:44 -0400

    add to reducer

--- src/components/game.js
+++ src/components/game.js
@@ -1,73 +1,19 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import Header from './header';
 import GuessSection from './guess-section';
 import StatusSection from './status-section';
 import InfoSection from './info-section';
 
-export class Game extends React.Component {
-  makeGuess(guess) {
-    guess = parseInt(guess, 10);
-    if (isNaN(guess)) {
-      this.setState({ feedback: 'Please enter a valid number' });
-      return;
-    }
-
-    const difference = Math.abs(guess - this.state.correctAnswer);
-
-    let feedback;
-    if (difference >= 50) {
-      feedback = 'You\'re Ice Cold...';
-    } else if (difference >= 30) {
-      feedback = 'You\'re Cold...';
-    } else if (difference >= 10) {
-      feedback = 'You\'re Warm.';
-    } else if (difference >= 1) {
-      feedback = 'You\'re Hot!';
-    } else {
-      feedback = 'You got it!';
-    }
-
-    this.setState({
-      feedback,
-      guesses: [...this.state.guesses, guess]
-    });
-
-    // We typically wouldn't touch the DOM directly like this in React
-    // but this is the best way to update the title of the page,
-    // which is good for giving screen-reader users
-    // instant information about the app.
-    document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
-  }
-
-  render() {
-    const { feedback, guesses, auralStatus } = this.state;
-    const guessCount = guesses.length;
-
-    return (
-      <div>
-        <Header
-          onRestartGame={() => this.restartGame()}
-          onGenerateAuralUpdate={() => this.generateAuralUpdate()}
-        />
-        <main role="main">
-          <GuessSection
-            feedback={feedback}
-            guessCount={guessCount}
-            onMakeGuess={guess => this.makeGuess(guess)}
-          />
-          <StatusSection guesses={guesses} 
-            auralStatus={auralStatus}
-          />
-          <InfoSection />
-        </main>
-      </div>
-    );
-  }
+export default function Game(props) {
+  return (
+    <div>
+      <Header />
+      <main role="main">
+        <GuessSection />
+        <StatusSection />
+        <InfoSection />
+      </main>
+    </div>
+  );
 }
-
-
-
-// export default connect(mapStateToProps)(Game);
-
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -17,6 +17,37 @@ export default (state = initialState, action) => {
         });
     }
 
+    if (action.type === MAKE_GUESS) {
+        let feedback, guess;
+        guess = parseInt(guess, 10);
+        if (isNaN(guess)) {
+            feedback = 'Please enter a valid number';
+            return Object.assign({}, state, {
+                feedback,
+                guesses: [...state.guesses, guess]
+            });
+        }
+        
+        const difference = Math.abs(guess - this.state.correctAnswer);
+
+        if (difference >= 50) {
+            feedback = 'You\'re Ice Cold...';
+        } else if (difference >= 30) {
+            feedback = 'You\'re Cold...';
+        } else if (difference >= 10) {
+            feedback = 'You\'re Warm.';
+        } else if (difference >= 1) {
+            feedback = 'You\'re Hot!';
+        } else {
+            feedback = 'You got it!';
+        }
+        
+        return Object.assign({}, state, {
+            feedback,
+            guesses: [...state.guesses, guess]
+        });
+    }
+
     if (action.type === GENERATE_AURAL_UPDATE) {
         const { guesses, feedback } = this.state;
     
@@ -29,7 +60,7 @@ export default (state = initialState, action) => {
         if (guesses.length > 0) {
           auralStatus += ` ${pluralize ? 'In order of most- to least-recent, they are' : 'It was'}: ${guesses.reverse().join(', ')}`;
         }
-        
+
         return Object.assign({}, state, { auralStatus });
       }
 
