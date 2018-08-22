e2385972791de68dad10e33128a71f5d36331ff8
rosaxny <rosaxny@gmail.com>
2018-08-22 08:58:07 -0400

    set up reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import { RESTART_GAME, MAKE_GUESS, GENERATE_AURAL_UPDATE } from './actions';
+
+const initialState = {
+    guesses: [],
+    feedback: 'Make your guess!',
+    auralStatus: '',
+    correctAnswer: Math.floor(Math.random() * 100) + 1
+};
+
+export default (state = initialState, action) => {
+    if (action.type === RESTART_GAME) {
+        return Object.assign({}, state, {
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '',
+            correctAnswer: action.correctAnswer
+        });
+    }
+}
